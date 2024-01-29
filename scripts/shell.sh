b59c941a0597119f49fb3bd44ff90d7bb320e7fb
Constantin Hentgen <constantin.hentgen@proton.me>
2024-01-29 20:17:31 +0100

    uri fixed

--- scripts/shell.sh
+++ scripts/shell.sh
@@ -5,8 +5,7 @@ chsh -s $(which zsh)
 
 # ZSH theme
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
-echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
-
+echo 'source ~/.oh-my-zsh/custom/themes/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
 
 # auto-suggestion
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
