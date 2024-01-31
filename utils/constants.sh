b485de5dcd9f732e7127f5725693ec41373a91b5
Constantin Hentgen <constantin.hentgen@proton.me>
2024-01-31 16:25:17 +0100

    path corrected

--- utils/constants.sh
+++ utils/constants.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-$REPO_DIR="$HOME/$USER/Dotfiles/"
+$REPO_DIR="$HOME/Dotfiles/"
 $REPO_DOT_CONFIG_DIR="$REPO_DIR/.config/"
 $REPO_CONFIG_SCRIPTS_DIR="$REPO_DIR/config_scripts/"
 $REP_SYSTEMD_FILES="$REPO_DIR/systemd_files/"
