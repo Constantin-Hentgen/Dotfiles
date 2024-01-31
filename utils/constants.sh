eb6243c2e5543a99e4ed4a07f91c8f516f626943
Constantin Hentgen <constantin.hentgen@proton.me>
2024-01-31 16:31:48 +0100

    variable path fixed

--- utils/constants.sh
+++ utils/constants.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-$REPO_DIR="$HOME/Dotfiles/"
-$REPO_DOT_CONFIG_DIR="$REPO_DIR/.config/"
-$REPO_CONFIG_SCRIPTS_DIR="$REPO_DIR/config_scripts/"
-$REP_SYSTEMD_FILES="$REPO_DIR/systemd_files/"
+REPO_DIR="$HOME/Dotfiles/"
+REPO_DOT_CONFIG_DIR="$REPO_DIR/.config/"
+REPO_CONFIG_SCRIPTS_DIR="$REPO_DIR/config_scripts/"
+REP_SYSTEMD_FILES="$REPO_DIR/systemd_files/"
