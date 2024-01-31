b942b80be43a26936910dc411724e04cfba30d30
Constantin Hentgen <constantin.hentgen@proton.me>
2024-01-31 21:23:17 +0100

    i3.sh improved

--- config_scripts/i3.sh
+++ config_scripts/i3.sh
@@ -8,18 +8,33 @@ source "$HOME/Dotfiles/utils/constants.sh"
 # Configure i3lock-color
 configure_i3lock_color() {
     local repo_dir="$HOME/$USER/i3lock-color"
-    clone_git_repo "https://github.com/Raymo111/i3lock-color.git" "$repo_dir"
+
+    # Check if the repository directory already exists
+    if [ -d "$repo_dir" ]; then
+        echo "i3lock-color repository already exists at $repo_dir. Skipping clone."
+    else
+        clone_git_repo "https://github.com/Raymo111/i3lock-color.git" "$repo_dir"
+    fi
+
     "$repo_dir/install-i3lock-color.sh"
 }
 
 # Configure xborder
 configure_xborder() {
     local repo_dir="$HOME/$USER/xborder"
-    clone_git_repo "https://github.com/deter0/xborder" "$repo_dir"
+
+    # Check if the repository directory already exists
+    if [ -d "$repo_dir" ]; then
+        echo "xborder repository already exists at $repo_dir. Skipping clone."
+    else
+        clone_git_repo "https://github.com/deter0/xborder" "$repo_dir"
+    fi
+
     chmod +x "$repo_dir/xborders"
     pip install -r "$repo_dir/requirements.txt"
     "$repo_dir/xborders" --help
 }
+
 
 # i3 configuration
 link_dir "$REPO_DOT_CONFIG_DIR/i3" "$HOME/.config/i3"
@@ -35,7 +50,7 @@ link_dir "$REPO_DOT_CONFIG_DIR/polybar" "$HOME/.config/polybar"
 
 # xborder configuration
 configure_xborder
-link_dir "$REPO_DIR/xborder/config.json" "$HOME/border/config.json"
+link_dir "$REPO_DIR/xborder/config.json" "$HOME/xborder/config.json"
 
 # rofi configuration
 link_dir "$REPO_DIR/rofi" "$HOME/.config/rofi"
